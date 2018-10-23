14d8adbc69241476aafa1c2c590b5034c771314d
Aravindkumar99 <38681824+Aravindkumar99@users.noreply.github.com>
2018-10-23 20:24:49 +0530

    Add files via upload

---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/untitled0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  2 12:14:04 2018
+
+@author: Administrator
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+salary=pd.read_csv('Salary_Data.csv')
